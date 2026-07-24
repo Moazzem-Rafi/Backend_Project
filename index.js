d43df194a1e179339d5152b553ab6bcfc3c09560
Moazzem-Rafi <mhr8june@gmail.com>
2026-07-24 21:33:19 +0600

    server and database connected

---
+++ index.js
@@ -0,0 +1,20 @@
+require('node:dns').setServers(['1.1.1.1','8.8.8.8'])
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://backendProject:KT3M7yS2GLg32qAo@cluster0.dzmv75i.mongodb.net/backendProject?appName=Cluster0",
+).then(()=>{
+    console.log('Database Connected');
+    
+}).catch((err)=>{
+console.log('Database Connection Error',err);
+
+});
+
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("Server is Running");
+});
