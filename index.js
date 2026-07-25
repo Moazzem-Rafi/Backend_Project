1f5c982ee320b190f1624590b62b2ad4dd7369ae
Moazzem-Rafi <mhr8june@gmail.com>
2026-07-26 01:45:05 +0600

    faaah

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 mongoose.connect(
   "mongodb+srv://backendProject:KT3M7yS2GLg32qAo@cluster0.dzmv75i.mongodb.net/backendProject?appName=Cluster0",
 ).then(()=>{
-    console.log('Database Connected');
+    console.log('Database Connected successfully!');
     
 }).catch((err)=>{
 console.log('Database Connection Error',err);
